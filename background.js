8082a7bec7ab112e6e85712c5ddf1b103ceb9e65
Marissa Biescker <marissa.biesecker@gmail.com>
2022-04-14 17:50:01 -0500

    Revert renaming of the background worker

--- background.js
+++ background.js
@@ -2,6 +2,6 @@ let isDoggos = false;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({isDoggos: false}, function() {
-    console.log('Not newsworthy Doggos-- yet!');
+    console.log('No newsworthy Doggos-- yet!');
   });
 });
